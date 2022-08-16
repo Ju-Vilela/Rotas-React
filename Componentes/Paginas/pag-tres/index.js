dee75dbcd668dae40d87027c26168df6604f08a5
Julya <86087548+Ju-Vilela@users.noreply.github.com>
2022-08-16 20:17:09 -0300

    Update index.js

--- Componentes/Paginas/pag-tres/index.js
+++ Componentes/Paginas/pag-tres/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Info02() {
+export default function PagTres() {
     return (
 
         <div className="container">
 
             <h1 className='texto'>JavaScript</h1>
 
-            //⚠ Não se põe imagem assim no React ⚠
             <img src="https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_vscode_icon_130084.png"></img>
 
             <label className='legenda'>Escolha o Link:</label>
