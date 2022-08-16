29a835d98b94bf6eb080191da0b38ed267fca872
Julya <86087548+Ju-Vilela@users.noreply.github.com>
2022-08-16 17:54:09 -0300

    Pagina 01
    Mostra os links para as outras paginas e uma pequena imagem do bloco de notas.
    ⚠ "Img" feito de forma errada ⚠

---
+++ Componentes/Paginas/pag-um/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './pag-um.css';
+
+export default function Pag-Um() {
+  return (
+
+    <div className="container">
+
+      <h1 className='texto'>HTML</h1>
+
+      //⚠ Não se põe imagem assim no React ⚠
+      <img src="https://cdn130.picsart.com/326989740015211.png"></img>
+
+      <label className='legenda'>Escolha o Link:</label>
+
+      <div className='links'>
+
+        <Link className='pag-um' to='/pag-um'>CSS</Link>
+        <Link className='pag-dois' to='/pag-dois'>JS</Link>
+
+      </div>
+      
+    </div>
+  );
+}
