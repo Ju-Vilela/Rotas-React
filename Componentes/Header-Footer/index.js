ffa903bb2cd29676952792862c98ade78cf8d7b3
Julya <86087548+Ju-Vilela@users.noreply.github.com>
2022-08-16 17:41:57 -0300

    Arquivo Informativo
    Onde fica meu cabeçalho e rodapé.

---
+++ Componentes/Header-Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './component.css'
+
+export function Footer() {
+    return (
+      <footer>&#120148;&#120160;&#120158; React</footer>
+    );
+}
+
+export function Header() {
+    return (
+        <header>@&#120129;&#120144;&#120141;&#120131;&#120131;</header>
+    );
+}
