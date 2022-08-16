c67d633b73191efc9aaf470a676bcd4b100aec7e
Julya <86087548+Ju-Vilela@users.noreply.github.com>
2022-08-16 18:01:58 -0300

    Pagina 03
    Mostra os links e uma imagem no visual studio code
    ⚠ "Img" feito de forma errada ⚠

---
+++ Componentes/Paginas/pag-tres/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Info02() {
+    return (
+
+        <div className="container">
+
+            <h1 className='texto'>JavaScript</h1>
+
+            //⚠ Não se põe imagem assim no React ⚠
+            <img src="https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_vscode_icon_130084.png"></img>
+
+            <label className='legenda'>Escolha o Link:</label>
+
+            <div className='links'>
+
+                <Link className='pag-um' to='/'>HTML</Link>
+                <Link className='pag-dois' to='/pag-dois'>CSS</Link>
+
+            </div>
+
+        </div>
+
+  );
+}
